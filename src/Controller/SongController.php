ab2752ae5befc96b47a461cd672ccac9325d2dac
Reuben Walker, Jr <rwalker@mobile-atom.com>
2022-04-23 17:47:10 -0400

    Chapter 14
    Ch 14 commit

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controller;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -7,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class SongController extends AbstractController
 {
     #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
         $song = [
@@ -15,6 +16,11 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
+
         return $this->json($song);
     }
 }
